ff6dbd09f38fa9dfe8f8be8b694f25bcfd8f1310
George <geosem042@gmail.com>
2023-12-10 18:39:44 +0200

    AppLayout Cleanup

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -54,22 +54,6 @@ const logout = () => {
 							<span
 								class="self-center text-2xl text-indigo-900 font-semibold whitespace-nowrap dark:text-white">Pet Care</span>
 						</a>
-						<form action="#" method="GET" class="hidden md:block md:pl-2">
-							<label for="topbar-search" class="sr-only">Search</label>
-							<div class="relative sm:w-64 md:w-96">
-								<div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-									<svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-										viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-										<path fill-rule="evenodd" clip-rule="evenodd"
-											d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z">
-										</path>
-									</svg>
-								</div>
-								<input type="text" name="email" id="topbar-search"
-									class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-									placeholder="Search" />
-							</div>
-						</form>
 					</div>
 					<div class="flex items-center lg:order-2">
 						
@@ -170,7 +154,7 @@ const logout = () => {
 						</li>
 					</ul>
 				</div>
-				
+
 				<!-- Bottom Sidebar -->
 				<!-- <div class="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-slate-950 dark:bg-gray-800 z-20">
 					
